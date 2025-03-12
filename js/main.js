d28a0c6c81f790cc875e51872517bd03abc3bb50
amol9145 <amol@koinetmedia.com>
2025-03-12 11:52:21 +0530

    New

---
+++ js/main.js
@@ -0,0 +1,174 @@
+(function($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function() {
+        setTimeout(function() {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
+
+    // Dropdown on mouse hover
+    const $dropdown = $(".dropdown");
+    const $dropdownToggle = $(".dropdown-toggle");
+    const $dropdownMenu = $(".dropdown-menu");
+    const showClass = "show";
+
+    $(window).on("load resize", function() {
+        if (this.matchMedia("(min-width: 992px)").matches) {
+            $dropdown.hover(
+                function() {
+                    const $this = $(this);
+                    $this.addClass(showClass);
+                    $this.find($dropdownToggle).attr("aria-expanded", "true");
+                    $this.find($dropdownMenu).addClass(showClass);
+                },
+                function() {
+                    const $this = $(this);
+                    $this.removeClass(showClass);
+                    $this.find($dropdownToggle).attr("aria-expanded", "false");
+                    $this.find($dropdownMenu).removeClass(showClass);
+                }
+            );
+        } else {
+            $dropdown.off("mouseenter mouseleave");
+        }
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Back to top button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        dots: true,
+        loop: true,
+        center: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+
+
+    // Vendor carousel
+    $('.vendor-carousel').owlCarousel({
+        loop: true,
+        margin: 45,
+        dots: false,
+        loop: true,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive: {
+            0: {
+                items: 2
+            },
+            576: {
+                items: 4
+            },
+            768: {
+                items: 6
+            },
+            992: {
+                items: 8
+            }
+        }
+    });
+
+})(jQuery);
+
+
+// contact form logic
+document.getElementById('contactForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    // Gather form data
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    const phone = document.getElementById('phone').value;
+    const companyName = document.getElementById('companyName').value;
+    const countryCode = document.getElementById('countryCode').value;
+
+    // Create an object to hold the data
+    const formData = {
+        name: name,
+        email: email,
+        message: message,
+        phone: phone,
+        companyName: companyName,
+        countryCode: countryCode
+    };
+
+    try {
+        // Send the data to the backend using Fetch API
+        const response = await fetch('https://koinetwebbackend.onrender.com/api/postContact', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
+
+        if (response.ok) {
+            alert('Thank you for your message!');
+            // Clear the form fields
+            document.getElementById('contactForm').reset();
+        } else {
+            const errorData = await response.json();
+            alert('Error: ' + errorData.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('An error occurred while sending your message. Please try again later.');
+    }
+});
+
+// blog page code
